2d0c6f2e8c58400814741b99c8864c027d1bc606
Iman Crawford <ilcrawford@gmail.com>
2020-05-02 10:36:50 -0500

    Adding new project to test channels as replacement for TinyMessenger

---
+++ ChannelTester/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ChannelTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ch = Channel.CreateUnbounded<string>();
+            var consumer = Task.Run(async () =>
+            {
+                while (await ch.Reader.WaitToReadAsync())
+                    Console.WriteLine(await ch.Reader.ReadAsync());
+            });
+            var producer = Task.Run(async () =>
+            {
+                var rnd = new Random();
+                for (int i = 0; i < 5; i++)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(rnd.Next(3)));
+                    await ch.Writer.WriteAsync($"Message {i}");
+                }
+                ch.Writer.Complete();
+            });
+
+
+            Task.WhenAll(producer, consumer).Wait();
+            //Console.ReadLine();
+        }
+    }
+}
