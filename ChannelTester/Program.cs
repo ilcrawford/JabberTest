b26e4dd8041f8baf490efa1a61121587d685e2ae
Iman Crawford <ilcrawford@gmail.com>
2020-05-02 11:04:46 -0500

    Added a generator toe ChannelTester

--- ChannelTester/Program.cs
+++ ChannelTester/Program.cs
@@ -6,28 +6,47 @@ namespace ChannelTester
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
+        {
+            var joe = CreateMessenger("Joe", 5);
+            await foreach (var item in joe.ReadAllAsync()) Console.WriteLine(item);
+        }
+
+        
+        static ChannelReader<string> CreateMessenger(string msg, int count)
         {
             var ch = Channel.CreateUnbounded<string>();
-            var consumer = Task.Run(async () =>
+            var rnd = new Random();
+
+            Task.Run(async () =>
             {
-                while (await ch.Reader.WaitToReadAsync())
-                    Console.WriteLine(await ch.Reader.ReadAsync());
-            });
-            var producer = Task.Run(async () =>
-            {
-                var rnd = new Random();
-                for (int i = 0; i < 5; i++)
+                for (int i = 0; i < count; i++)
                 {
+                    await ch.Writer.WriteAsync($"{msg} {i}");
                     await Task.Delay(TimeSpan.FromSeconds(rnd.Next(3)));
-                    await ch.Writer.WriteAsync($"Message {i}");
                 }
                 ch.Writer.Complete();
             });
 
+            return ch.Reader;
+        }
 
-            Task.WhenAll(producer, consumer).Wait();
-            //Console.ReadLine();
+        static ChannelReader<T> Merge<T>(ChannelReader<T> first, ChannelReader<T> second)
+        {
+            var output = Channel.CreateUnbounded<T>();
+
+            Task.Run(async () =>
+            {
+                await foreach (var item in first.ReadAllAsync())
+                    await output.Writer.WriteAsync(item);
+            });
+            Task.Run(async () =>
+            {
+                await foreach (var item in second.ReadAllAsync())
+                    await output.Writer.WriteAsync(item);
+            });
+
+            return output;
         }
     }
 }
